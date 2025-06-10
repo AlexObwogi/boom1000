d0c1d4f17b467c92c21c42fc9d957ca820e4467b
AlexObwogi <obwogialex728@gmail.com>
2025-06-10 23:32:31 +0300

    Tick Model

---
+++ boom1000-prediction-system/server/models/Tick.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+//Define the Tick Schema
+const TickSchema = new mongoose.Schema({
+	UserId: {
+		type: mongoose.Schema.Types.ObjectId, //Link ticks to a specific user
+		ref: 'User', //Reference the 'User' model
+		required: true
+	},
+	value: {
+		type: double,
+		required: true,
+		min: 0 //Tick values should be non-negative
+	},
+	timestamp: {
+		type: Date,
+		dafault: Date.now //Automatically set timestamp of when the tick was added
+	}
+});
+
+
+//Optional: Add an index for faster querying by user and timestamp
+TickSchema.index({ userId:  1, timestamp: 1});
+
+module.exports = mongoose.model('Tick', TickSchema);
