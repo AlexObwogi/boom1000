8a86be9dd5a99db1f03f82b90fa0b33ad2f8fa05
AlexObwogi <obwogialex728@gmail.com>
2025-06-12 12:18:49 +0300

    Authentication Ticks function

---
+++ boom1000-prediction-system/client/src/api/ticks.js
@@ -0,0 +1,40 @@
+import axios form 'axios';
+
+const API_URL = process.env.REACT_APP_SERVER_URL + '/api/ticks';
+
+//Function to fetch all ticks for the current user
+export const getTicks = async (token) => {
+	try {
+		const res = await axios.get(API_URL, {
+			headers: { 'x-auth-token': token } // send token for authentication
+		});
+		return res.data; // Returns an array of tick objects
+	} catch (err) {
+		throw err.response.data.msg || err.message;
+	}
+};
+
+//Functions to add a new tick for the current user
+export const addTick = async (value, token) => {
+	try {
+		const res = await axios.post(API_URL, { value }, {
+			headers: { 'x-auth-token': token } // Send token for authentication
+		});
+		return res.data; // Returns the newly added tick object
+	} catch (err) {
+		throw err.response.data.msg || err.message;
+	}
+};
+
+//Function to delete all ticks foe the current user
+export const deleteAllTicks = async (token) => {
+	try {
+		const res = await axios.delete(`${API_URL}/all`, {
+			headers: { 'x-auth-token': token } // SEnd token for authentication
+		});
+		return res.data; // Returns { msg }
+	} catch (err) {
+		throw err.response.data.msg || err.message;
+	}
+};
+
